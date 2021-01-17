fa8ae5d12e83feb421502677aa30b35967a88b2f
Vcuber <31827964+Vcuber@users.noreply.github.com>
2021-01-17 21:50:31 +0530

    Update firebaseConfig.js
    Edited the oopsies

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCMgnxY_xgt_tQQsyvNSpo1Q3fVYl3Ef58",
-    authDomain: "timetrack-fbf0c.firebaseapp.com",
+    apiKey: "insert key here",
+    authDomain: "oof",
     projectId: "timetrack-fbf0c",
-    storageBucket: "timetrack-fbf0c.appspot.com",
-    messagingSenderId: "930370631522",
-    appId: "1:930370631522:web:db5c6c66e6c04664151e3b",
+    storageBucket: "bleh",
+    messagingSenderId: "daddy",
+    appId: "speankme",
     measurementId: "G-NKLDMMPTRR"
 };
 
